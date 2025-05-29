a46270e2a6033aa16a718325538c03cb9ce877f8
macaroni2008 <164804517+macaroni2008@users.noreply.github.com>
2025-05-29 05:36:16 +0300

    Add files via upload

--- Praktika5/src/main/java/org/example/BonusService.java
+++ Praktika5/src/main/java/org/example/BonusService.java
@@ -2,9 +2,9 @@ package org.example;
 
 public class BonusService {
     public long calculate(long amount, boolean registered) {
-        int percent = registered ? 5 : 1;  // 5% для зарегистрированных, 1% для остальных
+        int percent = registered ? 5 : 1;
         long bonus = amount * percent / 100;
-        long limit = 500;  // максимальный бонус
-        return Math.min(bonus, limit);  // возвращаем меньшую из величин: бонус или лимит
+        long limit = 500;
+        return Math.min(bonus, limit);
     }
 }
