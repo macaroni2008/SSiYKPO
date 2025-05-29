3d5a72f94a8138dcaa0e0551b56ec904c4e1f25c
macaroni2008 <164804517+macaroni2008@users.noreply.github.com>
2025-05-29 05:39:55 +0300

    Add files via upload

--- Praktika6_3/src/main/java/ru/netology/services/VacationService.java
+++ Praktika6_3/src/main/java/ru/netology/services/VacationService.java
@@ -2,20 +2,15 @@ package ru.netology.services;
 
 public class VacationService {
     public int calculate(int income, int expenses, int threshold) {
-        int count = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
+        int count = 0;
+        int money = 0;
 
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) {
-                // Режим отдыха:
-                // 1. Сначала вычитаем expenses
-                // 2. Затем уменьшаем остаток в 3 раза
                 money -= expenses;
                 money /= 3;
                 count++;
             } else {
-                // Режим работы:
-                // income - expenses
                 money += income - expenses;
             }
         }
